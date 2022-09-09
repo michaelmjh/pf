4a46623bef01eb40ebe798ace84f52fa4b9a7235
michaelmjh <74016689+michaelmjh@users.noreply.github.com>
2022-09-09 09:42:55 +0100

    added poject experience

--- client/src/components/mainProjects.js
+++ client/src/components/mainProjects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Baton from '../media/images/Baton.png'
+import Dillon from '../media/images/dillon.jpeg'
 import TopTrumps from '../media/images/toptrumps.png'
 
 const MainProjects = () => {
@@ -14,7 +15,7 @@ const MainProjects = () => {
             <p class='subheadtagline'>CAPSTONE GROUP PROJECT</p>
             <p>An iOS app used to teach music theory through interactive lessons and quizzes, as well as allowing users 
               <br/>to create and edit profiles.
-              <br/>Technologies Used:
+              <br/>Built using:
               <br/> - Dart
               <br/> - Flutter
               <br/> - Java
@@ -31,7 +32,7 @@ const MainProjects = () => {
             <p>An app that allows users to play a version of the card game, Top Trumps. In addition to playing the game, 
               <br/>users can also create and update profiles and custom decks of cards, as well as see play statistics 
               <br/>and league tables.
-              <br/>Technologies Used:
+              <br/>Built using:
               <br/> - JavaScript
               <br/> - React
               <br/> - Express.js
@@ -40,6 +41,25 @@ const MainProjects = () => {
               <br/> - Insomnia
               <br/> - HTML
               <br/> - CSS
+              
+            </p>
+          </div>
+      </div>
+      <div class='mainprojectscontentleft'>
+          <img class='mainprojectsimage' src={Dillon} width='200' height='130'></img>
+          <div>
+            <p class='subhead'>project_gym</p>
+            <p class='subheadtagline'>Solo Project</p>
+            <p>A web application designed to manage fitness classes and member bookings for a gym built with an MVC 
+              <br/>design pattern.
+
+              <br/>Built using:
+              <br/> - Python3
+              <br/> - Flask
+              <br/> - SQLite3
+              <br/> - HTML
+              <br/> - CSS
+
               
             </p>
           </div>
